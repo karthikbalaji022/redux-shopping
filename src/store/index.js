bb151c0cc4bb818f7643deb82188a938bc58e1c5
karthikbalaji022 <karthikbalaji022@gmail.com>
2022-04-15 23:45:56 +0530

    added reducer

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from "redux";
+var initialValue={
+    count:0
+}
+const reducer=(state={initialValue}, action)=>
+{
+switch(action.type)
+{
+    default:
+        return state
+}
+}
+
+const store=createStore(reducer)
+export default store
